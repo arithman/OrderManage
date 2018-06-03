338d4beefb5fa7bcf5297400072e681b02bde417
陈涛 <arithman007@163.com>
2018-06-03 20:16:38 +0800

    修改窗口标题，并更改默认主窗口大小。

---
+++ OrderManage/ShowOrderPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OrderManage
+{
+    /// <summary>
+    /// ShowOrderPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class ShowOrderPage : Page
+    {
+        public ShowOrderPage()
+        {
+#pragma warning disable CS0103 // 当前上下文中不存在名称“InitializeComponent”
+            InitializeComponent();
+#pragma warning restore CS0103 // 当前上下文中不存在名称“InitializeComponent”
+        }
+    }
+}
